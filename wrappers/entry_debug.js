610f96e5429c8d746be3f841508cafc77eda1e11
Jazz <jazzzz@gmail.com>
2013-04-30 14:37:18 +0200

    Fixed one line offset between Chrome and real line numbers

--- wrappers/entry_debug.js
+++ wrappers/entry_debug.js
@@ -1,3 +1,4 @@
-require.define($__filename,Function(['require','module','exports','__dirname','__filename','process','global'],$body
-));
+require.define($__filename,function(require,module,exports,__dirname,__filename,process,global){
+    eval($body)
+});
 require($__filename);
